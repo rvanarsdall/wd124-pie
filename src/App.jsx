d1b810b05e0ab802bce97759b74be0b27c798f9c
Rob Vanarsdall <rvanar@gmail.com>
2022-01-31 14:27:57 -0500

    mounted pgAboutme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import PgHeader from "./components/pg-header/PgHeader";
+import PgAboutMe from "./components/pg-challenge/PGAboutMe";
 
 function App() {
   let name = "Rob";
@@ -9,6 +10,7 @@ function App() {
       <PgHeader />
       <h1>Welcome to React, {name}</h1>
       <h2>We just Modified our root App Component</h2>
+      <PgAboutMe />
     </div>
   );
 }
