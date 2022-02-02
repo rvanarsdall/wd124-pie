1019dfd293527f2078ce8860de5a0f9c6488312d
Rob Vanarsdall <rvanar@gmail.com>
2022-02-02 14:32:18 -0500

    toggle complete

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,12 +1,28 @@
+import React, { useState } from "react";
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 
 const Auth = (props) => {
+  const [isLoginVisible, setIsLoginVisible] = useState(true);
+
+  function handleToggle() {
+    // if (isLoginVisible === true) {
+    //   setIsLoginVisible(false);
+    // } else {
+    //   setIsLoginVisible(true);
+    // }
+    setIsLoginVisible(!isLoginVisible);
+  }
   return (
     <div>
       <h1>Hello from Auth</h1>
-      <Signup updateLocalStorage={props.updateLocalStorage} />
-      <Login updateLocalStorage={props.updateLocalStorage} />
+      {isLoginVisible === true ? (
+        <Login updateLocalStorage={props.updateLocalStorage} />
+      ) : (
+        <Signup updateLocalStorage={props.updateLocalStorage} />
+      )}
+      <br />
+      <button onClick={handleToggle}>Toggle Login/Sigup</button>
     </div>
   );
 };
