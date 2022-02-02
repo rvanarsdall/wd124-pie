8d3d3c478a7e33cbe3888fca01c075eea9f223c3
Rob Vanarsdall <rvanar@gmail.com>
2022-02-02 11:40:25 -0500

    added signup

--- src/components/Auth/Signup/Signup.jsx
+++ src/components/Auth/Signup/Signup.jsx
@@ -1,9 +1,35 @@
 //? firstName, lastName, email, password
 import React, { useState } from "react"; //imrs
+import { APIURL, EndPoints } from "../../endpoints";
 
 const Signup = (props) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("button was clicked");
+    console.log(APIURL + EndPoints.user.register);
+    const reqestObject = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    };
+
+    fetch(APIURL + EndPoints.user.register, {
+      method: "POST",
+      body: JSON.stringify(reqestObject),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }
   return (
     <div>
       <h1>Hello from Signup</h1>
@@ -26,6 +52,29 @@ const Signup = (props) => {
           value={lastName}
           onChange={(e) => setLastName(e.target.value)}
         />
+        <br />
+        <label htmlFor="email">Email Name</label>
+        <br />
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+        <label htmlFor="passwrd">Password</label>
+        <br />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit" onClick={handleSubmit}>
+          Submit Signup
+        </button>
       </form>
     </div>
   );
---
+++ src/components/endpoints.js
@@ -0,0 +1,7 @@
+export const EndPoints = {
+  user: {
+    register: "/user/register",
+  },
+};
+
+export const APIURL = "http://localhost:4000";
