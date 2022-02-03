97dc87167385085a606b41b55b11cdace5317f08
Rob Vanarsdall <rvanar@gmail.com>
2022-02-03 11:33:33 -0500

    pie inputs updated

---
+++ src/components/PieIndex/PieCreate/PieCreate.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+//? nameOfPie, baseOfPie, crust, timeToBake, servings, ratings  we need to create useState variables for these inputs
+//TODO Create a form
+//TODO Have a function for submitting data
+//TODO make sure we have a _____ when we submit the pie
+//TODO make sure we are sending numbers not strings
+
+const PieCreate = (props) => {
+  const [nameOfPie, setNameOfPie] = useState("");
+  const [baseOfPie, setBaseOfPie] = useState("");
+  const [crust, setCrust] = useState("");
+  const [timeToBake, setTimeToBake] = useState("");
+  const [ratings, setRatings] = useState("");
+  const [servings, setServings] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("it worked ");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name of Pie"
+          value={nameOfPie}
+          onChange={(e) => setNameOfPie(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Base of Pie"
+          value={baseOfPie}
+          onChange={(e) => setBaseOfPie(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Crust"
+          value={crust}
+          onChange={(e) => setCrust(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Time to Bake"
+          value={timeToBake}
+          onChange={(e) => setTimeToBake(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Rating"
+          value={ratings}
+          onChange={(e) => setRatings(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Servings"
+          value={servings}
+          onChange={(e) => setServings(e.target.value)}
+        />
+        <br />
+        <button>Click to Submit a Pie</button>
+      </form>
+    </div>
+  );
+};
+
+export default PieCreate;
