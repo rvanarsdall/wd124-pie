5dad123722437476a9ffc3c4d30b68815157656f
Rob Vanarsdall <rvanar@gmail.com>
2022-02-03 11:34:18 -0500

    pie index updated

--- src/components/PieIndex/PieIndex.jsx
+++ src/components/PieIndex/PieIndex.jsx
@@ -1,7 +1,10 @@
+import PieCreate from "./PieCreate/PieCreate";
+
 const PieIndex = (props) => {
   return (
     <div>
       <h2>Hello from Pie Index</h2>
+      <PieCreate />
     </div>
   );
 };
